b8fb53ecbc665629fe008b967a92a570f89ba27e
Sherry <sherryptk@gmail.com>
2017-09-19 17:32:55 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 25; i++) {
+  if (i===1) {console.log('I am 1 strange loop.')
+} else {
+  console.log('I am ${i} strange loops.')
+}
+}
+
+// If your i value is 1, add the string "I am 1 strange loop."; 
+// if your i value is anything else, add the string "I am ${i} strange loops."
